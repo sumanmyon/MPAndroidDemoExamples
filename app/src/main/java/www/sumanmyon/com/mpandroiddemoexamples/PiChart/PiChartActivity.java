e1d91e48adfba7f9e9d9957496bbb91715af7584
Suman Neupane <sumanmyon.sam@gmail.com>
2019-05-08 16:54:55 +0545

    Creating PiChart Activity and Class

---
+++ app/src/main/java/www/sumanmyon/com/mpandroiddemoexamples/PiChart/PiChartActivity.java
@@ -0,0 +1,15 @@
+package www.sumanmyon.com.mpandroiddemoexamples.PiChart;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import www.sumanmyon.com.mpandroiddemoexamples.R;
+
+public class PiChartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pi_chart);
+    }
+}
